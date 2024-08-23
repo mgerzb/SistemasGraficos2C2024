692224e5c30d68194b618457c96c63d5a445e985
Marco-GG <marco.gerzb@gmail.com>
2024-08-23 01:12:14 -0300

    Nuevas partes a la escena del castillo
    * Agregue torres y puerta al castillo
    * Agregue lago frente al castillo

--- Ejercicios/E0_Castillo/src/sceneManager.js
+++ Ejercicios/E0_Castillo/src/sceneManager.js
@@ -11,33 +11,87 @@ export class SceneManager {
 		light.position.set(1, 5, 1);
 		scene.add(light);
 
-		cone = new THREE.Mesh(new THREE.ConeGeometry(1, 2, 126, 100), new THREE.MeshPhongMaterial({ color: 0x00ffff }));
-		cone.position.set(3, 1, 0);
-		scene.add(cone);
-
 		this.addAxesHelper();
 		this.buildCastle();
 	}
 
 	buildCastle()
 	{
-		// Suelo
+		// Soil
 		const planeGeometry = new THREE.PlaneGeometry(50,50);
-		const planeMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00, side: THREE.SingleSide});
+		const planeMaterial = new THREE.MeshBasicMaterial({color: 0x005500, side: THREE.SingleSide});
 		const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 		plane.position.set(0,0,0);
 		plane.rotation.x = -Math.PI/2;
 		this.baseScene.add(plane);
 
 		// Castle - main
-		cube = new THREE.Mesh(new THREE.BoxGeometry(5, 2, 5), new THREE.MeshPhongMaterial({ color: 0xffffbb }));
+		const castleMaterial = new THREE.MeshPhongMaterial({ color: 0xffffbb });
+		cube = new THREE.Mesh(new THREE.BoxGeometry(5, 2, 5), castleMaterial);
 		cube.position.set(0, 1, -5);
 		this.baseScene.add(cube);
 
 		// Castle - Towers
+		const towerGeometry = new THREE.CylinderGeometry(0.9,0.9,2.4,32);
+		const towerA = new THREE.Mesh(towerGeometry, castleMaterial);
+		const towerB = new THREE.Mesh(towerGeometry, castleMaterial);
+		const towerC = new THREE.Mesh(towerGeometry, castleMaterial);
+		const towerD = new THREE.Mesh(towerGeometry, castleMaterial);
+		
+		
+		towerA.position.set(2.5, 1.2, -2.5);
+		towerB.position.set(-2.5, 1.2, -2.5);
+		towerC.position.set(-2.5, 1.2, -7.5);
+		towerD.position.set(2.5, 1.2, -7.5);
 
-
-
+		this.baseScene.add(towerA);
+		this.baseScene.add(towerB);
+		this.baseScene.add(towerC);
+		this.baseScene.add(towerD);
+		
+		// Castle -- Towers Roof
+		
+		const towerRoofGeometry = new THREE.ConeGeometry(1.2, 2.3, 126, 100);
+		const towerRoofMaterial = new THREE.MeshPhongMaterial({ color: 0x003377 });
+		
+		const towerRoofA = new THREE.Mesh(towerRoofGeometry, towerRoofMaterial);
+		const towerRoofB = new THREE.Mesh(towerRoofGeometry, towerRoofMaterial);
+		const towerRoofC= new THREE.Mesh(towerRoofGeometry, towerRoofMaterial);
+		const towerRoofD = new THREE.Mesh(towerRoofGeometry, towerRoofMaterial);
+		
+		towerRoofA.position.set(2.5, 3.55, -2.5);
+		towerRoofB.position.set(-2.5, 3.55, -2.5);
+		towerRoofC.position.set(-2.5, 3.55, -7.5);
+		towerRoofD.position.set(2.5, 3.55, -7.5);
+		
+		this.baseScene.add(towerRoofA);
+		this.baseScene.add(towerRoofB);
+		this.baseScene.add(towerRoofC);
+		this.baseScene.add(towerRoofD);
+		
+		// Castle -- Door
+		
+		const doorMaterial = new THREE.MeshPhongMaterial({ color: 0xaf6955 });
+		const Door = new THREE.Mesh(new THREE.BoxGeometry(0.75, 1, 0.05), doorMaterial);
+		Door.position.set(0, 0.5, -2.5);
+		this.baseScene.add(Door);
+		
+		// Lake
+		
+		const lakeGeometry = new THREE.CircleGeometry( 1, 32 ); 
+		const lakeMaterial = new THREE.MeshBasicMaterial( { color: 0x007775 } ); 
+		const lakeA = new THREE.Mesh( lakeGeometry, lakeMaterial );
+		const lakeB = new THREE.Mesh( lakeGeometry, lakeMaterial );
+		
+		lakeA.rotation.x = -Math.PI/2;
+		lakeA.position.set(0, 0.001, 3);
+		
+		lakeB.rotation.x = -Math.PI/2;
+		lakeB.position.set(0, 0.001, 1.5);
+		
+		this.baseScene.add(lakeA);
+		this.baseScene.add(lakeB);
+		
 		//Three
 
 	}
