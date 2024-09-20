57cf055af5c7e09fd7f90693f5af097cc842c632
Marco-GG <marco.gerzb@gmail.com>
2024-09-20 01:21:56 -0300

    Match vite config path with github pages

--- Ejercicios/E1_Transformaciones/vite.config.js
+++ Ejercicios/E1_Transformaciones/vite.config.js
@@ -4,5 +4,5 @@ export default defineConfig({
 	server: {
 		port: 10001, // Personaliza el puerto aquí
 	},
-	base: './', // Personaliza el directorio base de los links del HTML aquí
+	base: 'SistemasGraficos2C2024/Ejercicios/E1_Transformaciones', // Personaliza el directorio base de los links del HTML aquí
 });
